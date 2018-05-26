4837ebdbb6e29997b3766ae94175dce8d350528e
Karol Bukowiec <karol.bukowiec@portacapena.com>
2018-05-26 09:25:28 +0200

    Fix Node service init

--- Skrzynex/Startup.cs
+++ Skrzynex/Startup.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -23,6 +25,7 @@ namespace Skrzynex
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddNodeServices();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
